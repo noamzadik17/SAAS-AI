812c6b513854f1486ec987d38ff4618da8d84cbc
Noam Zadik <noamzadik17@gmail.com>
2024-03-27 21:17:39 +0200

    Almost Finished, Need Some Light Fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
-// import { Collection } from "@/components/shared/Collection"
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
-// import { getAllImages } from "@/lib/actions/image.actions"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = async () => {
-  // const page = Number(searchParams?.page) || 1;
-  // const searchQuery = (searchParams?.query as string) || '';
-  // { searchParams }: SearchParamProps
-  // const images = await getAllImages({ page, searchQuery })
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery })
 
   return (
     <>
@@ -33,12 +33,12 @@ const Home = async () => {
       </section>
 
       <section className="sm:mt-12">
-        {/* <Collection
+        <Collection
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
           page={page}
-        /> */}
+        />
       </section>
     </>
   )
